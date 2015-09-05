39ddd4fc112536f8ce961a85bd02f596cf7755e7
Joao Almeida <jmdalmeida92@gmail.com>
2015-09-05 21:59:08 +0100

    Fixed typo

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 INTERVAL = 60  # Routine sleep interval (in seconds).
 FILE = "sites.txt"  # File to get the site list from.
 DELIMITER = "|"  # Determines which string to use as delimiter when splitting sites.
-SAVE_DIFF = False # Wether to store the result of difflib.unified_diff.
+SAVE_DIFF = False # Whether to store the result of difflib.unified_diff.
 # Path for the new file.
 # $h - site's hostname; $t - current timestamp
 DIFF_FILENAME = "change_logs/$h/$t.log" 
